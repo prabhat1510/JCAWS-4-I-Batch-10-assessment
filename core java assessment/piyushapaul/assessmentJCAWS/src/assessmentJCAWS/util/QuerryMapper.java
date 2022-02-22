111f3af8c6b9ae070a5bfe012dfda90e13520f72
PiyushaPaul <piyushapaul1999@gmail.com>
2022-02-22 18:10:24 +0530

    Updated

--- core java assessment/piyushapaul/assessmentJCAWS/src/assessmentJCAWS/util/QuerryMapper.java
+++ core java assessment/piyushapaul/assessmentJCAWS/src/assessmentJCAWS/util/QuerryMapper.java
@@ -2,8 +2,8 @@ package assessmentJCAWS.util;
 
 public interface QuerryMapper {
 	
-	static final String REGISTER_USER = "insert into users(SerialNo.,username,password) values(?,?,?)";
-	static final String LOGIN_USER = "select * from users where username=? and userpassword=?";
+	static final String REGISTER_USER = "insert into user(SerialNo.,username,password) values(?,?,?)";
+	static final String LOGIN_USER = "select * from user where username=? and password=?";
 	static final String GET_ALL_USERS = "select * from user";
 
 }
