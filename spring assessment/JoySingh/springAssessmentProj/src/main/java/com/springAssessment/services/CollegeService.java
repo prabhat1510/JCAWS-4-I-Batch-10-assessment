03566f405b83628dc30c2f23d4427bc11e137ac4
Joy Singh <avnishmamta060606@gmail.com>
2022-03-14 17:24:19 +0530

    Joy Singh Spring Assessment

---
+++ spring assessment/JoySingh/springAssessmentProj/src/main/java/com/springAssessment/services/CollegeService.java
@@ -0,0 +1,26 @@
+package com.springAssessment.services;
+
+import java.util.List;
+
+import com.springAssessment.exception.CollegeNotFoundException;
+import com.springAssessment.models.College;
+
+public interface CollegeService {
+
+	public List<College> getAllColleges();
+
+	// CRUD
+	public College addCollege(College college);
+
+	public College getCollegeById(Integer collegeId) throws CollegeNotFoundException;
+
+	public Boolean updateCollegeDetails(College newCollege, Integer collegeId);
+
+	public Boolean deleteCollegeById(Integer collegeId);
+
+	// user should be able to search college by Name and address separately
+	public College getCollegeByName(String collegeName) throws CollegeNotFoundException;
+
+	public College getCollegeByAddress(String address) throws CollegeNotFoundException;
+
+}
---
+++ spring assessment/JoySingh/springAssessmentProj/src/test/java/com/springAssessment/controllers/ConsumerControllerTest.java
@@ -0,0 +1,14 @@
+package com.springAssessment.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConsumerControllerTest {
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
