d4ce65be0fc87e4074f3f2bf632d9112f3865ccd
rahulmalv <lovecr427@gmail.com>
2022-03-14 17:48:01 +0530

    Assesment

---
+++ spring assessment/Rahul malviya/springassesmentproject/src/main/java/springassesment/service/CollegeServiceImpl.java
@@ -0,0 +1,30 @@
+package springassesment.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import springassesment.entity.College;
+import springassesment.repository.CollegeRepository;
+
+@Service
+public class CollegeServiceImpl implements CollegeService {
+
+	@Autowired
+	CollegeRepository collegeRepository;
+
+	@Override
+	public College addCollege(College newcollege) {
+		return collegeRepository.save(newcollege);
+	}
+
+//	@Override
+//	public College retriveCollegeByName(String college) {
+//		return collegeRepository.findCollegeByName(college);
+//	}
+//
+//	@Override
+//	public College retriveCollegeByAddress(String address) {
+//		return collegeRepository.findCollegeByAddress(address);
+//	}
+	
+}
